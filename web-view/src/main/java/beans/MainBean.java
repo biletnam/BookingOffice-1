e702dc1170ea93474c297fb33a5efa71523da045
kitpes <n.kukushkina@gmail.com>
2014-01-12 00:23:35 +0200

    pom edited

--- web-view/src/main/java/beans/MainBean.java
+++ web-view/src/main/java/beans/MainBean.java
@@ -4,12 +4,17 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 
+import model.Flight;
+
+import org.eclipse.persistence.internal.queries.ArrayListContainerPolicy;
+
 
 public class MainBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Date filterDepartureDate;
 	private String filterArrival;
+	private ArrayList<Flight> listFlight;
 	
 	
 	
