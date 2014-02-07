9a9c1131ec398f84932f9a36bb535b660da91936
kitpes <n.kukushkina@gmail.com>
2014-02-07 16:05:49 +0200

    Customer service try

--- model/src/main/java/service/CustomerService.java
+++ model/src/main/java/service/CustomerService.java
@@ -1,36 +1,27 @@
 package service;
 
-import java.sql.Date;
-import java.util.List;
+import java.util.Map;
 
-import dao.*;
+//import dao.*;
 import entity.*;
 
 
 public class CustomerService {
-	private TicketDao1 ticketDao;
-	private ReservationDao1 reservationDao;
-	private FlightDao1 flightDao;
+//	private TicketDao ticketDao;
+//	private ReservationDao reservationDao;
+//	private FlightDao flightDao;
 	
-	public CustomerService(TicketDao1 ticketDao, ReservationDao1 reservationDao,
-			FlightDao1 flightDao) {
-		super();
-		this.ticketDao = ticketDao;
-		this.reservationDao = reservationDao;
-		this.flightDao = flightDao;
-	}
-
-	void bookTickets(List<Ticket> listT, Reservation r) {
-		reservationDao.create(r);
-		for (Ticket t : listT) {
-			t.setReservationId(r.getId());
-			t.setStatus(TicketStatus.BOOKED);
-			ticketDao.update(t);
-		}
+	void addReservation(Map<Flight, Integer> tickets) {
+//		reservationDao.create(r);
+//		for (Ticket t : listT) {
+//			t.setReservationId(r.getId());
+//			t.setStatus(TicketStatus.BOOKED);
+//			ticketDao.update(t);
+//		}
 	}
 	
-	List<Flight> findNecesseryFlights(String arrival, Date departureDate) {
-		return flightDao.find(arrival, departureDate);
-	}
+//	List<Flight> findNecesseryFlights(String arrival, Date departureDate) {
+//		return flightDao.find(arrival, departureDate);
+//	}
 	
 }
