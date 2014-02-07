c35d10dc4245b285dc617f1e9265b209a4753890
kitpes <n.kukushkina@gmail.com>
2014-02-07 15:54:29 +0200

    Security officer service

--- model/src/main/java/service/SecurityOfficerService.java
+++ model/src/main/java/service/SecurityOfficerService.java
@@ -1,50 +1,17 @@
 package service;
 
-import java.util.List;
-
-import dao.AccountDao1;
-import dao.AccountRightsDao1;
+import dao.AccountDao;
 import entity.Account;
-import entity.AccountRights;
-
 
 public class SecurityOfficerService {
-	private AccountDao1 accountDao;
-	private AccountRightsDao1 accountRightsDao;
-	
-	public SecurityOfficerService(AccountDao1 accountDao,
-			AccountRightsDao1 accountRightsDao) {
-		super();
-		this.accountDao = accountDao;
-		this.accountRightsDao = accountRightsDao;
-	}
-	
-	void createAccount(Account a, List<AccountRights> listAr) {
+	private AccountDao accountDao;
+
+	void addAccount(Account a) {
 		accountDao.create(a);
-		for (AccountRights ar : listAr) {
-			accountRightsDao.create(ar);
-		}
-		
 	}
-	
+
 	void editAccount(Account a) {
 		accountDao.update(a);
 	}
-	
-	void editAccountsRights(Account a, List<AccountRights> listArNew) {
-		List<AccountRights> listAr = accountRightsDao.getAccountRights(a);
-		for (AccountRights ar : listAr) {
-			accountRightsDao.delete(ar);
-		}
-		
-		for (AccountRights ar : listArNew) {
-			accountRightsDao.create(ar);
-		}
-	}
-	
-	void deactivateAccount(Account a) {
-		a.setActive(false);
-		accountDao.update(a);
-	}
-}
 
+}
