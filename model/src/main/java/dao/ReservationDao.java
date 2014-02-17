ffe5e6c9631d82c8bf23249e19061a40ee3813bc
kitpes <n.kukushkina@gmail.com>
2014-02-17 23:48:48 +0200

    accountant reservation

--- model/src/main/java/dao/ReservationDao.java
+++ model/src/main/java/dao/ReservationDao.java
@@ -41,14 +41,16 @@ public class ReservationDao extends GenericDaoImpl<Reservation> {
 	}
 
 	public List<Reservation> findActual() {
-		GregorianCalendar gc = (GregorianCalendar) Calendar.getInstance();
+		Date now = Calendar.getInstance().getTime();
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(now);
 		gc.add(Calendar.DAY_OF_YEAR, -3);
-		Date now = gc.getTime();
+		Date time = gc.getTime();
 		
-		TypedQuery<Reservation> query = entityManager.createQuery("SELECT r FROM Reservation r where r.dateReservation > ?1 and r.paid = false", Reservation.class);
+		TypedQuery<Reservation> query = entityManager.createQuery("SELECT r FROM Reservation r where r.dateReservation > ?1", Reservation.class);
 		List<Reservation> reservations = null;
 		
-		query.setParameter(1, now);
+		query.setParameter(1, time);
 		reservations = query.getResultList();	
 		
 		return reservations;	
